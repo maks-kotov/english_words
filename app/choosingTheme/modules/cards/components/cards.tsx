d3273c615dc3c8642579d1c8ad752ab86fbc8477
makson <maxsyper8902@gmail.com>
2026-06-24 00:22:03 +0500

    ура карточки готовы

--- app/choosingTheme/modules/cards/components/cards.tsx
+++ app/choosingTheme/modules/cards/components/cards.tsx
@@ -2,8 +2,49 @@ import Card from "./ui/card";
 
 export default function Cards(): React.ReactElement {
   return (
-    <div className="grid grid-cols-1 gap-8 mb-16">
-      <Card />
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-16">
+      <Card //я не должен хардкодить много карточек
+        icon="🐱"
+        title="Животные"
+        countWords={24}
+        progress="4/7"
+        repeatAfter={14}
+      />
+      <Card //я не должен хардкодить много карточек
+        icon="🐱"
+        title="Животные"
+        countWords={24}
+        progress="4/7"
+        repeatAfter={14}
+      />
+      <Card //я не должен хардкодить много карточек
+        icon="🐱"
+        title="Животные"
+        countWords={24}
+        progress="4/7"
+        repeatAfter={14}
+      />
+      <Card //я не должен хардкодить много карточек
+        icon="🐱"
+        title="Животные"
+        countWords={24}
+        progress="4/7"
+        repeatAfter={14}
+      />
+      <Card //я не должен хардкодить много карточек
+        icon="🐱"
+        title="Животные"
+        countWords={24}
+        progress="4/7"
+        repeatAfter={14}
+      />
+      <Card //я не должен хардкодить много карточек
+        icon="🐱"
+        title="Животные"
+        countWords={24}
+        progress="4/7"
+        repeatAfter={14}
+      />
     </div>
   );
 }
--- app/choosingTheme/modules/cards/components/ui/card.tsx
+++ app/choosingTheme/modules/cards/components/ui/card.tsx
@@ -1,13 +1,25 @@
 import { Button } from "@/components/ui/button";
-
-export default function Card(): React.ReactElement {
+interface props {
+  icon: string;
+  title: string;
+  countWords: number;
+  progress: string;
+  repeatAfter: number;
+}
+export default function Card({
+  icon,
+  title,
+  countWords,
+  progress,
+  repeatAfter,
+}: props): React.ReactElement {
   return (
     <div className="w-full border border-border py-8 px-6 rounded-lg">
-      <div className="mb-3 text-4xl">🐱</div>
-      <div className="mb-3 text-2xl font-bold">Животные</div>
-      <div className="mb-12 text-lg font-bold">24 слова</div>
-      <div className="mb-3 text-md">📊 Прогресс: 4/7</div>
-      <div className="mb-6 text-md">⏳ Повтор через: 14 дней</div>
+      <div className="mb-3 text-4xl">{icon}</div>
+      <div className="mb-3 text-2xl font-bold">{title}</div>
+      <div className="mb-12 text-lg font-bold">{countWords} слов</div>
+      <div className="mb-3 text-md">📊 Прогресс: {progress}</div>
+      <div className="mb-6 text-md">⏳ Повтор через: {repeatAfter} дней</div>
       <Button className="w-full text-lg">Выбрать</Button>
     </div>
   );
